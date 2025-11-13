b31db63836c0fb56eb7aa379be407622ed82ec0e
DiKan <1870408497@qq.com>
2025-11-13 17:11:30 +1100

    chore(catalog): tweak the json

--- app/(tabs)/constellation-chart/index.tsx
+++ app/(tabs)/constellation-chart/index.tsx
@@ -99,12 +99,4 @@ const styles = StyleSheet.create({
 });
 
 export default App;
-import { View, Text } from 'react-native';
 
-export default function ConsetellaPage() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>constellation</Text>
-    </View>
-  );
-}
