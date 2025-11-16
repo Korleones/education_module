b88fc21d53f7dff13dbc68cce644f6f558752579
DiKan <1870408497@qq.com>
2025-11-16 21:36:36 +1100

    tweak

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Redirect } from 'expo-router';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function Index() {
+  return <Redirect href="/home" />;
 }
-
